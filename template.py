37f1184929ebd8fb75216e6413274ac0600ed663
Purvesh Sohony <purvesh.sohony@newvisionsoftpune.com>
2025-01-07 16:36:44 +0530

    template fix

--- template.py
+++ template.py
@@ -6,6 +6,7 @@ logging.basicConfig(level=logging.INFO)
 project_name="mlproject"
 
 list_of_files={
+    f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
