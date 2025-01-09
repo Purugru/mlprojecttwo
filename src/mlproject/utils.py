a3b1bdd4121e61ba10a6092cf9e42d744dc7ad37
Purvesh Sohony <purvesh.sohony@newvisionsoftpune.com>
2025-01-09 14:44:56 +0530

    model training complete

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -1,9 +1,15 @@
-import pickle
-import sys
 import os
-import numpy as np
+import sys
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+import pandas as pd
+import pickle
+import numpy as np
+
 
 
 def save_object(file_path, obj):
@@ -15,5 +21,40 @@ def save_object(file_path, obj):
         with open(file_path,"wb") as file_obj:
             pickle.dump(obj, file_obj)
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param[model_name]
+
+            logging.info(f"Training model: {model_name} with parameters: {para}")
+            
+            gs = GridSearchCV(model, para, cv=3)
+
+            # Fit the GridSearchCV to find the best parameters
+            gs.fit(X_train, y_train)
+
+            logging.info(f"Best parameters for {model_name}: {gs.best_params_}")
+            
+            # Update the model with the best parameters
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
     except Exception as e:
         raise CustomException(e, sys)
